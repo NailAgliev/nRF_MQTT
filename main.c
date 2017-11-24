d0a49b21e7660de37c87337c3659593dcbcb2e2c
NailAgliev <aglievnail@gmail.com>
2017-11-25 04:04:03 +0500

    Переделка под ошибки

--- main.c
+++ main.c
@@ -78,14 +78,28 @@
 //#define 	APP_SCHED_BUF_SIZE(EVENT_SIZE, QUEUE_SIZE)   (((EVENT_SIZE) + APP_SCHED_EVENT_HEADER_SIZE) * ((QUEUE_SIZE) + 1))
 
 char modem_data[128];
-uint8_t modem_state = 0;
 
-typedef enum {
-	AT_CHECK,
-	REG_CHECK,
-		
-	
+void (*p_func)(void *p_event_data, uint16_t event_size);
+
+enum {
+	AT,
+	CFUN,
+	CFUN_1,
+	ATE,
+	ATV,
+	CMEE,
+	CPIN_CHECK,
+	CSQ_CHECK,
+	CREG_CHECK,
+	CIPSHUT,
+	CGTT_CHECK,
+	CIPQSEND,
+	CIPRXGET,
+	CSTT,
+	CIICR,
+	OK, //15
 } modem_int_state;
+
 
 
 static void scheduler_init(void)
@@ -94,17 +108,6 @@ static void scheduler_init(void)
 }
 
 
-uint8_t sms_flag = 0;
-
-
-void modem_resp ()
-{
-	if(modem_data[0] == 0x0)
-	{
-		modem_state = 0;
-	}
-	
-}
 
 
 
@@ -113,33 +116,29 @@ static void slep_handler()
 	SEGGER_RTT_printf(0, "got\r\n");
 }
 
-void serial_scheduled_ex (void *p_event_data, uint16_t event_size)
-{
-	if (modem_data[0] == 0)
-	{
-	nrf_serial_read(p_event_data, &modem_data, sizeof(modem_data), NULL, 0);
-	}
-	else
-	{
-		while(false)
-		{
-			uint8_t i = 0;
-			if (modem_data[i] == 0)
-			{
-				nrf_serial_read(p_event_data, &modem_data[i], sizeof(modem_data), NULL, 0);
-				i = 0;
-				break;
-			}
-			else
-			{
-				i++;
-			}
-		}
-	}
-	modem_resp();
-	
-}
 
+
+	
+//	if (modem_data[0] == 0)
+//	{
+//	nrf_serial_read(p_event_data, &modem_data, sizeof(modem_data), NULL, 0);
+//	}
+//	else
+//	{
+//		while(false)
+//		{
+//			uint8_t i = 0;
+//			if (modem_data[i] == 0)
+//			{
+//				nrf_serial_read(p_event_data, &modem_data[i], sizeof(modem_data), NULL, 0);
+//				i = 0;
+//				break;
+//			}
+//			else
+//			{
+//				i++;
+//			}
+//		}
 
 static void event_handler(struct nrf_serial_s const * p_serial, nrf_serial_event_t event)
 {
@@ -147,7 +146,7 @@ static void event_handler(struct nrf_serial_s const * p_serial, nrf_serial_event
 	{
 		case NRF_SERIAL_EVENT_RX_DATA:
 		{
-			app_sched_event_put(p_serial, sizeof(*p_serial), serial_scheduled_ex);
+			app_sched_event_put(p_serial, sizeof(*p_serial), p_func);
 		}
 		default:
 			break;
@@ -198,9 +197,12 @@ void at_write(char str[])
 {
 	
 	char at[2] = "AT";
-	
+	char n[2] = "\n";
 	char * data_tx = concat(at, str);
-		
+	data_tx = concat(data_tx, n);
+	
+	SEGGER_RTT_printf(0, "%s", data_tx);
+	
 	nrf_serial_write(&serial_uart, data_tx, sizeof(*data_tx), NULL, 0);
 	
 }
@@ -209,18 +211,162 @@ void at_write(char str[])
 
 
 
-
 void modem_init()
 {
-	at_write("\n");
 	
+	switch(modem_int_state)
+	{
+		case AT:  									 //Проверка доступен ли модуль
+			{
+				at_write("");
+			}
+		case CFUN:
+			{
+		
+			}
+		case CFUN_1: 								 //Рестарт модуля
+			{
+		
+			}
+		case ATE:										//No echo mode
+			{
+		
+			}
+		case ATV:										//Числовой формат ответов
+			{
+		
+			}
+		case CMEE:  							 	//Кодовый формат ошибок
+			{
+		
+			}
+		case CPIN_CHECK:						
+			{
+			
+			}
+		case CSQ_CHECK: 						//Проверка силы сигнала
+			{
+				
+			}
+		case CREG_CHECK:						//Проверка рекгестрации в сети
+			{
+				
+			}
+		case CIPSHUT:								//TCP restart
+			{
+				
+			}
+		case CGTT_CHECK:            //проверка готовности модуля для установления связи
+			{
+				
+			}
+		case CIPRXGET:							//Ручное получение полученных сообщений
+			{
+				
+			}
+		case CIPQSEND:							//Режим отправки без потверждения получения		
+			{
+				
+			}
+		case CSTT:									//Конфигурация APN
+			{
+				
+			}
+		case CIICR:									//Влючение GPRS
+			{
+				
+			}			
+	}
 }
+
+
+
+void serial_scheduled_ex (void *p_event_data, uint16_t event_size)
+{
+	if(modem_int_state < OK)
+	{
+		switch(modem_int_state)
+	{
+		case AT:  									 //Проверка доступен ли модуль
+			{
+				nrf_serial_read(p_event_data, &modem_data, sizeof(modem_data), NULL, 0);
+				if(modem_data == 0)
+				{
+					modem_int_state = CFUN;
+					modem_init();
+				}
+			}
+		case CFUN:
+			{
+		
+			}
+		case CFUN_1: 								 //Рестарт модуля
+			{
+		
+			}
+		case ATE:										//No echo mode
+			{
+		
+			}
+		case ATV:										//Числовой формат ответов
+			{
+		
+			}
+		case CMEE:  							 	//Кодовый формат ошибок
+			{
+		
+			}
+		case CPIN_CHECK:						
+			{
+			
+			}
+		case CSQ_CHECK: 						//Проверка силы сигнала
+			{
+				
+			}
+		case CREG_CHECK:						//Проверка рекгестрации в сети
+			{
+				
+			}
+		case CIPSHUT:								//TCP restart
+			{
+				
+			}
+		case CGTT_CHECK:            //проверка готовности модуля для установления связи
+			{
+				
+			}
+		case CIPRXGET:							//Ручное получение полученных сообщений
+			{
+				
+			}
+		case CIPQSEND:							//Режим отправки без потверждения получения		
+			{
+				
+			}
+		case CSTT:									//Конфигурация APN
+			{
+				
+			}
+		case CIICR:									//Влючение GPRS
+			{
+				
+			}			
+	}
+	}
+}
+
+
+
+
 
 
 int main(void)
 {
     ret_code_t ret;
 		
+		p_func = &serial_scheduled_ex;
+	
 		scheduler_init();
 	
 	
